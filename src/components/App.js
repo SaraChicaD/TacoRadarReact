d588aeb1adb793052159964fc828e405865ecd42
sarainéscalderón <sarainescalderon@sarainescalderons-MacBook-Air.local>
2016-07-10 22:37:57 -0500

    adding in draft of geolocation

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,27 @@ import Footer from './Footer';
 import Main from './Main';
 
 export default class App extends Component {
-  render() {
+
+componentDidMount() {
+
+	//If brower supports HTML5 geoLocation
+	if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(function(position) { 
+	    const lat = position.coords.latitude;
+	    const lng = position.coords.longitude;
+
+	    console.log('run');
+
+	  });
+	    
+	}
+	else {
+	  alert('This Browser doesn\'t support HTML5 geolocation');
+	}
+
+}
+
+render() {
     return (
 	  <div>
 	  	<Header />
@@ -12,5 +32,6 @@ export default class App extends Component {
       	<Footer />
       </div>
     );
-  }
+}
+
 }
